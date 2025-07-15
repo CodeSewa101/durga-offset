4b46f31a2caedfb87ec44f22660704b21b874e4f
GleanCoder <adityakiranacharya0@gmail.com>
2025-07-15 07:23:21 +0530

    overlay text is removed

--- src/components/sections/PrintingShop.jsx
+++ src/components/sections/PrintingShop.jsx
@@ -149,23 +149,13 @@ const PrintingShop = () => {
                         <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z" />
                       </svg>
                     </div>
-                    <div className="text-xs font-medium">
-                      Sample {currentImageIndex + 1}
-                    </div>
                   </div>
                 </div>
               </div>
 
               {/* Overlay with printing info */}
               <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent flex items-end">
-                <div className="text-white p-3 sm:p-4 w-full">
-                  <div className="text-sm font-medium mb-1">
-                    Sample {currentImageIndex + 1}
-                  </div>
-                  <div className="text-xs opacity-75">
-                    Premium Quality Print
-                  </div>
-                </div>
+                <div className="text-white p-3 sm:p-4 w-full"></div>
               </div>
             </div>
 
