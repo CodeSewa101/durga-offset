54be8cfc195f56dacce5b464d7c9acff19882810
Kanhu <kanhu2024@gmail.com>
2025-07-15 07:45:59 +0530

    some firebase configuration are added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: "./", // 🔥 This line is essential for Firebase
   plugins: [react(), tailwindcss()],
 });
